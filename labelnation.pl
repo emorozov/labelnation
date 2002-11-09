227300b95650732fffac41c0505f181d8f39e54d
kfogel <kfogel@d37bbd87-4cf8-0310-9f5b-e7918974f4aa>
2002-11-09 00:09:07 +0000

    Add Avery 5168 support.
    git-svn-id: http://svn.red-bean.com/repos/labelnation/trunk@100 d37bbd87-4cf8-0310-9f5b-e7918974f4aa

--- labelnation.pl
+++ labelnation.pl
@@ -117,6 +117,20 @@ sub set_parameters_for_type ()
     $params[8] = "Times-Roman";   # Font_Name
     $params[9] = 12;              # Font_Size
   }
+  elsif ($ntype eq "avery5168")
+  {
+    # Big shipping labels, 4 per page
+    $params[0] = 31;              # Left_Margin
+    $params[1] = 33;              # Bottom_Margin
+    $params[2] = 254;             # Label_Width
+    $params[3] = 363;             # Label_Height
+    $params[4] = 37;              # Horiz_Space
+    $params[5] = 0;               # Vert_Space
+    $params[6] = 2;               # Horiz_Num_Labels
+    $params[7] = 2;               # Vert_Num_Labels
+    $params[8] = "Times-Roman";   # Font_Name
+    $params[9] = 12;              # Font_Size
+  }
   elsif (($ntype eq "avery5160")
          or ($ntype eq "avery5260")
          or ($ntype eq "avery5560")
@@ -848,6 +862,8 @@ sub types ()
   ### equivalences in two places.  Make a data structure to hold all
   ### this, use it both here and in set_parameters_for_type().
   print "Predefined label types:\n";
+  print "\n";
+  print "   Avery-5168                             (4 labels per page)\n";
   print "\n";
   print "   Avery-5263, 5663, 5963                 (10 labels per page)\n";
   print "\n";
